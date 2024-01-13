36b9bfd4a4931a9f24e381c9846c6bd8b54d20e1
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-01-13 12:01:26 +0000

    Done Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,8 +28,44 @@ console.log(booleanIsLoggedIn);
 
 let stringNumber = 44
 let stringNumberInString = String(stringNumber)
-console.log(stringNumber);
-console.log(typeof stringNumberInString); // dikh number raha hai but wo string bnn gaya hai 
+//console.log(stringNumber);
+//console.log(typeof stringNumberInString); // dikh number raha hai but wo string bnn gaya hai 
+
+// main conversions are between bollean, number, string 
 
 
+// **************************** Operations *****************************
+
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+// 2 raise to the power 2 => (2**2) 
+// 2 is to 3 (2%3) 
+
+let str1 = "hello"
+let str2 = "shivani"
+
+let str3 = str1 + str2
+console.log(str3);  // we can only add
+
+console.log("1" + 2); // => 12
+console.log(1 + "2"); // => 12 
+
+console.log("1" + 2 + 2); // => 122
+console.log(1 + 2 + "2"); // => 32
+console.log( (3 + 4) % 5 * 6);  // add paranthesis to make it clear 
+
+console.log(true); // => true
+console.log(+true); // => 1 
+console.log(+""); // => 0 
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2 // not so usable
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
