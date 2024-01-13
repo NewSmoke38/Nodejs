b6cd3e809eee79c29f2af8221320c9f3b648fe73
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-01-13 12:25:43 +0000

    Done Comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+//console.log(2 > 1);
+//console.log(2 < 1);
+//console.log(2 >= 1);
+//console.log(2 <= 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// === means ki ab wo string ko number me nhi convert krega 
+console.log("2" === 2);
