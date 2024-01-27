4aede20a7984988305ea175a2704f886de30cc85
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-01-27 18:12:01 +0000

    Objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+// singleton
+
+// object literlas
+
+const JsUser = {
+  name: "Jonas"
+  age: 16,
+  location: "macau",
+  email: "hulu@example.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Tuesday",
+}
+// to access the object with dot 
+console.log(JsUser.email);
+console.log(JsUser["email"]);
