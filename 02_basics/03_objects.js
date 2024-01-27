72ff9894fbbe0ef751eaea687039c706fbfd19a2
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-01-27 18:20:02 +0000

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,13 +3,16 @@
 // object literlas
 
 const JsUser = {
-  name: "Jonas"
-  age: 16,
-  location: "macau",
+  name: "Jonas",
+  age: 30,
+
+  
+  
+  location: "london",
   email: "hulu@example.com",
   isLoggedIn: false,
   lastLoginDays: ["Monday", "Tuesday",
 }
 // to access the object with dot 
 console.log(JsUser.email);
-console.log(JsUser["email"]);
+//console.log(JsUser["email"]);
