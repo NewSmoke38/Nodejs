c0b4b54d34d037e5a3333a583da278e84249c14c
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-01-27 18:33:07 +0000

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,14 +5,12 @@
 const JsUser = {
   name: "Jonas",
   age: 30,
-
-  
-  
   location: "london",
   email: "hulu@example.com",
   isLoggedIn: false,
-  lastLoginDays: ["Monday", "Tuesday",
+  lastLoginDays: ["Monday", "Tuesday",]
 }
 // to access the object with dot 
-console.log(JsUser.email);
-//console.log(JsUser["email"]);
+//console.log(JsUser.email);
+console.log(JsUser["email"]);
+
