5878d2b7884358fcc5854e67a71d8de75e40e275
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-01-28 16:05:53 +0000

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,15 +2,40 @@
 
 // object literlas
 
+const mySym = Symbol('key1') // to make this a symbol put it in square barcjets.
+
 const JsUser = {
   name: "Jonas",
+  
+  "full name": "Jonas Schmedtmann",
+  
+  [mySym]: "myKey1", // to make it a symbol always put it in square brackets. that is syntax.
+  
   age: 30,
+  
   location: "london",
+  
   email: "hulu@example.com",
+  
   isLoggedIn: false,
   lastLoginDays: ["Monday", "Tuesday",]
 }
 // to access the object with dot 
-//console.log(JsUser.email);
-console.log(JsUser["email"]);
+console.log(JsUser.email); // asked ki email kya hai with dot method
+console.log(JsUser["email"]); // but this is preferable
+// console.log(JsUser.full name); // because here you cant acces full name by dot syntax. this is wrong
+console.log(JsUser["full name"]); // this is correct
+
+// for accesing symbol.  
+
+// symbol is not symbol here until i put it in square brackets in line 12.
+console.log(JsUser["mySym"]); // gives myKey1
+console.log(typeof JsUser.mySym); // gives string which is wrong
+console.log(typeof JsUser[mySym]); // gives symbol this is correct form.
+
+//  how to change values in email. if you want nobody to change values for JsUserthe use freeze.
+
+  
+
+
 
