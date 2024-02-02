d48a53ae24ae64432845920a3de52e3a88536fc6
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-02-02 18:41:50 +0000

    Obj.

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,6 +1,6 @@
 // on singletons
 const tinderUser = new Object();
-const tinderUser = {}; // both gives same answer
+// const tinderUser = {}; // both gives same answer
 
 tinderUser.id = "123abc";
 tinderUser.name = "Jonas";
@@ -10,12 +10,12 @@ tinderUser.isLoggedIn = false;
 
 const regularUser = {
   email: "qpmzj@example.com",
-  fullName: {
+    fullName: {
        userfullname:{
            userFirstName: "Jonas",
            userLastName: "Schmedtmann"
       }
-  }
+    }
 }
 
 console.log(regularUser.fullName.userfullname.userFirstName); // gives first name in the nesting
@@ -25,4 +25,8 @@ console.log(regularUser.fullName.userfullname.userFirstName); // gives first nam
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
 
-const obj3 = {...obj1, ...obj2}; 
+//const obj3 = {...obj1, ...obj2}; // ye replit me aisa hi hai, baaki in vid it is {obj1, obj2}
+const obj3 = Object.assign({}, obj1, obj2); // this is the right way always to get a nice nested ans.
+
+// mainly the thing happening here is that obj1 and obj2 are getting in the {} empty parenthesis. but if i dont use {} then all values will go straight into obj1.
+console.log(obj3); // gives {1: "a", 2: "b", 3: "c", 4: "d"} nested answer.
