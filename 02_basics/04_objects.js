cfcc8d143fb7bccca5eff401dd30e56b9835e8ff
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-02-03 18:37:47 +0000

    Obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -25,8 +25,31 @@ console.log(regularUser.fullName.userfullname.userFirstName); // gives first nam
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
 
-//const obj3 = {...obj1, ...obj2}; // ye replit me aisa hi hai, baaki in vid it is {obj1, obj2}
+//const obj3 = {...obj1, ...obj2}; // ye best hai known as spread operater.
 const obj3 = Object.assign({}, obj1, obj2); // this is the right way always to get a nice nested ans.
 
 // mainly the thing happening here is that obj1 and obj2 are getting in the {} empty parenthesis. but if i dont use {} then all values will go straight into obj1.
 console.log(obj3); // gives {1: "a", 2: "b", 3: "c", 4: "d"} nested answer.
+
+
+// now data about many users will come to me in array form which will contain many objects
+const users = [
+  {
+    id: 1,
+    email: "qpmzj@example.com",
+  },
+  {
+    id: 1,
+    email: "qpmzj@example.com",
+  },
+  {
+    id: 1,
+    email: "qpmzj@example.com",
+  },
+]
+
+// how to access
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // gives all keys in the object.;
