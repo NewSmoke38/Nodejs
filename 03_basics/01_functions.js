6e892a181b68d58a22cb4eebc8cad9a0b059d2a4
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-02-06 18:24:12 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+// i want to write this all with less effort 
+
+function sayMyName(){
+console.log("s");
+console.log("h");
+console.log("i");
+console.log("v");
+console.log("a");
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+   
+   console.log(number1 + number2);
+}
+
+// addTwoNumbers(3, 5); // gives 8
+// addTwoNumbers(2, "q"); // gives 2q
+console.log("Result: ", result);
+
