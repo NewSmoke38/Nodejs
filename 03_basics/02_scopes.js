dc33ba08aa3b2b662928d2889b90ad2155642a78
NewSmoke38 <142609266+NewSmoke38@users.noreply.github.com>
2024-02-11 17:43:19 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,11 +4,54 @@ if(true){
   let a = 10
   const b = 20
   // var c = 30 // var is not good at all
-  console.log("INNER: ", a);
+  //console.log("INNER: ", a);
 
-}
+} // this curly bracket is the block scope
 
 
-console.log(a); // print 300 so let is good
+//console.log(a); // print 300 so let is good
 //console.log(b);
 //console.log(c); // it prints 30 
+
+// nested scopes
+
+function one(){
+  const username = "Jonas"
+
+  function two(){
+    const website = "youtube"
+    console.log(username);
+  }
+ // console.log(website);
+
+  two()
+}
+
+//one()
+
+
+
+
+
+// global scope ka console block scope se kuch nhi le skta 
+// means parent child se nhi lega
+
+if(true) {
+ 
+  const username = "Jonas"
+  if (username === "Jonas"){
+    const website = "youtube"
+    console.log(username + website);
+   }
+  
+// console.log(website); // this wont work because website is not in the global scope
+}
+
+// console.log(username);// this wont work because username is not in the global scope
+
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++ INTERESTING +++++++++++++++++++++++++++++++++++++++
