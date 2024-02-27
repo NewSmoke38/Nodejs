20bec9293eec8759e3d7ee9c8b1a5fda4024bca8
ShivaniFrontend <142609266+NewSmoke38@users.noreply.github.com>
2024-02-27 18:27:50 +0000

    Truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,7 @@
+const userEmail = "jonas.ai"
+
+if (userEmail) { // agar user ka email hai toh got user email
+  console.log("Got user email");
+} else { // nhi toh ye likhdo
+  console.log("Dont have user email");
+}
